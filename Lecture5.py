ade7c09cb3cb6e1cd82e56aa13d7078dfe649a62
Ammar Bahman <bahman.ammar@gmail.com>
2016-06-23 04:54:43 -0400

    update more problems

--- Lecture5.py
+++ Lecture5.py
@@ -56,3 +56,21 @@ def inc(a):
     return a+1
 
 print applyEachTo([inc, square, halve, abs], -3)
+
+####problem in lecture 6
+def biggest(aDict):
+    '''
+        aDict: A dictionary, where all the values are lists.
+        
+        returns: The key with the largest number of values associated with it
+        '''
+    res = {}
+    for i in aDict:
+        res[i]=len(aDict[i])
+
+    for j in res.keys():
+        if res[j] == max(res.values()):
+            return j
+
+
+print biggest({'a': [15, 13, 12], 'b': [13, 6]})
