b4d79f1fe97c7e919be0f19931819303acf4d0c4
Ammar Bahman <bahman.ammar@gmail.com>
2016-07-01 05:36:51 -0400

    update error file

--- Error.py
+++ Error.py
@@ -19,3 +19,18 @@ def divideNew(x, y):
         print "result is", result
     finally:
         print "executing finally clause"
+
+
+
+def Normalize(numbers):
+    max_number = max(numbers)
+    assert(max_number != 0), "Cannot divide by 0"
+    for i in range(len(numbers)):
+        numbers[i]  /= float(max_number)
+        assert(0.0 <= numbers[i] <= 1.0), "output not between 0 and 1"
+    return numbers
+
+try:
+    Normalize([0, 0, 0])
+except ZeroDivisionError, e:
+    print 'Invalid maximum element'
